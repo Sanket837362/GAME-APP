5caec54969997c1f500fb02197661ac285048b1e
Sanket837362 <sanketukani309@gmail.com>
2024-03-25 12:53:14 +0530

    sd

--- user_registration/game.py
+++ user_registration/game.py
@@ -8,38 +8,36 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 def calculate_winning_number(bet):
     color_and_number_of_balls = {
-        "0": ["red", "violet", "small"],
-        "1": ["green", "small"],
-        "2": ["red", "small"],
-        "3": ["green", "small"],
-        "4": ["red", "small"],
-        "5": ["green", "violet", "big"],
-        "6": ["red", "big"],
-        "7": ["green", "big"],
-        "8": ["red", "big"],
-        "9": ["green", "big"],
+        "0": [ 'red', 'violet','small' ],
+        "1": [ 'green','small' ],
+        "2": [ 'red' ,'small'],
+        "3": [ 'green' ,'small'],
+        "4": [ 'red' ,'small'],
+        "5": [ 'green', 'violet','big' ],
+        "6": [ 'red' ,'big'],
+        "7": [ 'green' ,'big'],
+        "8": [ 'red' ,'big'],
+        "9": [ 'green' ,'big']
     }
-
-    total_amount = sum([b["total_amount"] for b in bet])
-    winning_amount = total_amount 
-    while True:
-        winning_number = str(random.randint(0, 9))
+    
+    amount = sum([b['total_amount'] for b in bet])
+    winning_amount = amount 
+    lowest_bet_dict  = {}
+    for i in range(0,10):
+        winning_number = str(i)
         ball = color_and_number_of_balls[winning_number]
         amount = 0
         for b in bet:
-            if b["type_of_bet"] == "big/small" and b["bet"] == ball[-1]:
-                amount += b["total_amount"] * 2
-            elif b["type_of_bet"] == "color" and b["bet"] in ball[0:-1]:
-                amount += (
-                    b["total_amount"] * 2
-                    if b["bet"] != "violet"
-                    else b["total_amount"] * 4.5
-                )
-            elif b["type_of_bet"] == "number" and str(b["bet"]) == str(winning_number):
-                amount += b["total_amount"] * 9
+            if b['type_of_bet'] == 'big/small' and b['bet'] == ball[-1]:
+                amount += b['total_amount'] * 2
+            elif b['type_of_bet'] == 'color' and b['bet'] in ball[0:-1]:
+                amount += b['total_amount'] * 2 if b['bet'] != 'violet' else b['total_amount'] * 4.5
+            elif b['type_of_bet'] == 'number' and str(b['bet']) == str(winning_number):
+                amount += b['total_amount'] * 9        
         if amount <= winning_amount:
-            break
-    return winning_number, winning_amount, amount, ball[0:-1], ball[-1]
+            lowest_bet_dict[winning_number] = amount
+    winning_number = min(lowest_bet_dict, key=lowest_bet_dict.get)
+    return winning_number, lowest_bet_dict[winning_number] , winning_amount , color_and_number_of_balls[winning_number][0:-1] , color_and_number_of_balls[winning_number][-1]
 
 
 def play_1min_game(game_id , game__type):
